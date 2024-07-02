7ef20b107051c19fff49d9df4fd78da34fe4c61a
Yahya Erdogan <yahyaerdoan@gmail.com>
2024-07-02 14:24:55 -0500

    Layers created. Core packages service implemented.

---
+++ MetroMiles.DomainLayer/Entities/Brand.cs
@@ -0,0 +1,24 @@
+﻿using Core.PersistenceLayer.Repositories.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetroMiles.DomainLayer.Entities;
+
+public class Brand : Entity<Guid>
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public Brand()
+    {
+
+    }
+    public Brand(Guid id, string name, string description)
+    {
+        Id = id;
+        Name = name;
+        Description = description;
+    }   
+}
