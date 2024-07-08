29a69ec6bcbf361de5f4a74997e2d64550de62b8
Yahya Erdogan <yahyaerdoan@gmail.com>
2024-07-07 23:54:46 -0500

    Some entities developed.

---
+++ MetroMiles.DomainLayer/Entities/Enums/CarStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetroMiles.DomainLayer.Entities.Enums
+{
+    public enum CarStatus
+    {
+        Available = 1,
+        Rented = 2,
+        UnderMaintenance = 3,
+    }
+}
